aab37ea4c228a31e57a1188f0eaf8aff8ac70716
Smiley <connor.smiley@uoit.net>
2019-03-24 20:48:18 -0400

    A* finished. Let's see if it works!

--- GameTest/Pathing.cpp
+++ GameTest/Pathing.cpp
@@ -5,9 +5,10 @@
 struct CPathNode {
 	CPathNode(const Cell& cell) : xIndex(cell.first), yIndex(cell.second) {}
 	float f() { return g + h; }
-	float g, h;
-	int xIndex;
-	int yIndex;
+	int g = 1;
+	float h = 0.0f;
+	int xIndex = 0;
+	int yIndex = 0;
 	CPathNode* parent = nullptr;
 
 	bool operator==(const CPathNode& pathNode) {
@@ -15,7 +16,7 @@ struct CPathNode {
 	}
 };
 
-std::vector<CPathNode> getNeighbours(const CSimpleTileMap& map, int xIndex, int yIndex) {
+inline std::vector<CPathNode> getNeighbours(const CSimpleTileMap& map, int xIndex, int yIndex) {
 	std::vector<CPathNode> neighbours;
 	const static char numAdjacent = 4;
 	CPathNode pendingNeighbours[numAdjacent] = {
@@ -35,6 +36,11 @@ std::vector<CPathNode> getNeighbours(const CSimpleTileMap& map, int xIndex, int 
 	return neighbours;
 }
 
+//Manhattan distance as my heuristic.
+inline float hValue(const Cell& cell1, const Cell& cell2) {
+	return abs(cell2.first - cell1.first) + abs(cell2.second - cell1.second);
+}
+
 std::vector<Cell> Pathing::aStar(const CSimpleTileMap& map, const Cell & start, const Cell & end)
 {
 	std::vector<CPathNode> closedList;
@@ -50,6 +56,7 @@ std::vector<Cell> Pathing::aStar(const CSimpleTileMap& map, const Cell & start, 
 				bestNode = node;
 		}
 
+		//Traverse the list of path nodes from end to start then reverse it!
 		if (bestNode.xIndex == end.first && bestNode.yIndex == end.second) {
 			std::vector<Cell> path;
 			while (bestNode.parent) {
@@ -65,6 +72,44 @@ std::vector<Cell> Pathing::aStar(const CSimpleTileMap& map, const Cell & start, 
 		closedList.push_back(bestNode);
 		openList.erase(std::find(openList.begin(), openList.end(), bestNode));
 
+		//Fetch surrounding nodes.
+		std::vector<CPathNode> neighbours = getNeighbours(map, bestNode.xIndex, bestNode.yIndex);
 
+		for (CPathNode& node : neighbours) {
+			//Make sure current node hasn't already been processed (ensure not in closed list).
+			if (std::find(closedList.begin(), closedList.end(), node) != closedList.end())
+				continue;
+
+			//Find the best scoring neighbour.
+			int gScore = bestNode.g + 1;
+			bool gScoreIsBest = false;
+
+			//Figure out if we've already processed it.
+			bool onList = false;
+			for (CPathNode& openNode : openList) {
+				if (openNode == node)
+					onList = true;
+			}
+
+			//If we're dealing with a new node, its guaranteed to be the best.
+			if (!onList) {
+				gScoreIsBest = true;
+				node.h = hValue(std::make_pair(node.xIndex, node.yIndex), end);
+				openList.push_back(node);
+			}
+
+			//Otherwise, re-evaluate its g score.
+			else if (gScore < node.g)
+				gScoreIsBest = true;
+
+			//Store the list leading up to the best node so far.
+			if (gScoreIsBest) {
+				node.parent = &bestNode;
+				node.g = gScore;
+			}
+		}
 	}
+
+	//Path finding failed if we've hit this point.
+	return std::vector<Cell>{};
 }
