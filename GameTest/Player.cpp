ecb97ef8bd3af1260fc1c95983f05857c4a5353d
Connor Smiley <connor.smiley@uoit.net>
2021-10-16 13:03:35 -0400

    Fixed player ricochet

--- GameTest/Player.cpp
+++ GameTest/Player.cpp
@@ -14,52 +14,35 @@ CPlayer::~CPlayer()
 
 void CPlayer::Update(const CSimpleTileMap& map, float deltaTime)
 {
-	//Main polls for 4 controllers and updates.
+	//CSimpleControllers polls for 4 controllers and updates. GetController() defaults to the 0th controller.
 	const CController& controller = CSimpleControllers::GetInstance().GetController();
 	deltaTime /= 1000.0f;
 	const float rotationVelocity = m_rotationSpeed * deltaTime;
 
-	//if(App::IsKeyPressed(Keys::A))
 	if(controller.CheckButton(XINPUT_GAMEPAD_DPAD_LEFT, false))
 		m_angle -= rotationVelocity;
-	//else if (App::IsKeyPressed(Keys::D))
 	else if (controller.CheckButton(XINPUT_GAMEPAD_DPAD_RIGHT, false))
 		m_angle += rotationVelocity;
 
 	const float translationVelocity = m_translationSpeed * deltaTime;
 	const CPoint direction{ Math::direction(m_angle) };
 	CPoint translation{ direction * translationVelocity };
-	//if (App::IsKeyPressed(Keys::W))
 	if (controller.CheckButton(XINPUT_GAMEPAD_DPAD_UP, false))
 		translation *= 1.0f;
-	//else if (App::IsKeyPressed(Keys::S))
 	else if (controller.CheckButton(XINPUT_GAMEPAD_DPAD_DOWN, false))
 		translation *= -1.0f;
 	else
 		translation *= 0.0f;
 
-	//printf("px: %f py: %f dx: %f dy: %f ox: %f oy: %f.\n", m_position.x, m_position.y, destination.x, destination.y, Math::overlap(destination.x, direction.x, map.getTileWidth()), Math::overlap(destination.y, direction.y, map.getTileHeight()));
+	//Negate the translation component if it will move the player to an invalid tile.
 	CPoint destination{ m_position + translation };
-	EMapValue destinationValue = map.GetTileMapValue(destination.x, destination.y);
-	if (destinationValue == EMapValue::BORDER || destinationValue == EMapValue::WALL || destinationValue == EMapValue::OUTOFBOUNDS) {
-		//Broken ricochet algorithm :(
-		/*const float tileWidth = map.getTileWidth();
-		const float tileHeight = map.getTileHeight();
-
-		const int xIndex = direction.x >= 0.0f ? floor(destination.x / tileWidth) : ceil(destination.x / tileWidth);
-		const int yIndex = direction.y >= 0.0f ? floor(destination.y / tileHeight) : ceil(destination.y / tileHeight);
-
-		const float xEdge = xIndex * tileWidth;
-		const float yEdge = yIndex * tileHeight;
-
-		float xTranslation = destination.x - xEdge;
-		float yTranslation = destination.y - yEdge;
-
-		destination.y -= yTranslation;
-		destination.x -= xTranslation;*/
-	}
-	else
-		m_position = destination;
+	EMapValue xTile = map.GetTileMapValue(destination.x, m_position.y);
+	EMapValue yTile = map.GetTileMapValue(m_position.x, destination.y);
+	if (yTile == EMapValue::BORDER || yTile == EMapValue::WALL || yTile == EMapValue::OUTOFBOUNDS)
+		destination.y -= translation.y;
+	if (xTile == EMapValue::BORDER || xTile == EMapValue::WALL || xTile == EMapValue::OUTOFBOUNDS)
+		destination.x -= translation.x;
+	m_position = destination;
 
 	//Clamp the angle between 0 and 360 (prevents overflow and gives me nice numbers).
 	m_angle += 360.0f;
